6cedf21612d8f3fa3f026825d73d4ace32507ce2
singhJasmeet159 <singhjasmeet2506@gmail.com>
2021-10-10 16:50:14 +1100

    working get driver information

--- api/config.php
+++ api/config.php
@@ -1,11 +1,11 @@
 <?php
- 
+
 /*
  * All database connection variables
  */
- 
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "s1ngh123"); // db password (mention your db password here)
+
+define('DB_USER', "hevo"); // db user
+define('DB_PASSWORD', "Hevo159"); // db password (mention your db password here)
 define('DB_DATABASE', "linehaulGSheets"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_SERVER', "192.168.20.200"); // db server
 ?>
--- api/getDriverData.php
+++ api/getDriverData.php
@@ -2,24 +2,22 @@
 
 $response = array();
 
-require_once __DIR__ . '/connect.php';
+require __DIR__ . '/config.php';
 
-$db = new DB_CONNECT();
+$db = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 
 if (isset($_GET["lisense_no"])){
     $lisense_no = $_GET["lisense_no"];
-
-    $result = $db->query("SELECT first_name, middle_name, last_name, email, lisense_no FROM drivers_drivers WHERE driver_status = 'current' and lisense_no = $lisense_no");
-
+    $result = mysqli_query($db, "SELECT first_name, middle_name, last_name, email, licence_no FROM drivers_drivers WHERE driver_status = 'current' and licence_no = $lisense_no");
     if(!empty($result)){
-        while($row = $result->fetch_assoc()){
+        while($row = mysqli_fetch_array($result)){
             $driver = array();
 
             $driver["first_name"] = $row["first_name"];
             $driver["middle_name"] = $row["middle_name"];
             $driver["last_name"] = $row["last_name"];
             $driver["email"] = $row["email"];
-            $driver["lisense_no"] = $row["lisense_no"];
+            $driver["licence_no"] = $row["licence_no"];
 
             $response["success"] = 1;
 
